5c163857e5b800af2b9f11c8f1280934a3020c93
epicmine99 <sean.ryan@mail.utoronto.ca>
2022-09-15 17:18:05 -0400

    Updated firebaseConfig

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { initializeApp } from 'firebase/app';
 
-const firebaseApp = initializeApp();
+const firebaseConfig = {
+    apiKey: "AIzaSyAu5c52G6uB8dI99Fg3cwIqvDt9GV9aVkY",
+    authDomain: "sean-s-web-hub.firebaseapp.com",
+    projectId: "sean-s-web-hub",
+    storageBucket: "sean-s-web-hub.appspot.com",
+    messagingSenderId: "134718887536",
+    appId: "1:134718887536:web:02d77b0f38ca47288dc6b5"
+}
+const firebaseApp = initializeApp(firebaseConfig);
